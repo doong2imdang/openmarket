04e794055044f29f866e3108edbd0089721f903f
KIm Seo Young <jmh7367@naver.com>
2023-07-08 00:26:05 +0900

    Feat: Router 설정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
+import SignupPage from "../pages/SignupPage";
 
 export default function Router() {
-  return <div>Router</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/loginpage" element={<LoginPage />} />
+      <Route path="/signuppage" element={<SignupPage />} />
+    </Routes>
+  );
 }
