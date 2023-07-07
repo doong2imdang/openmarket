860f83547d371ac0c006d910ec440f9605e236f6
KIm Seo Young <jmh7367@naver.com>
2023-07-08 00:28:48 +0900

    Feat: Input 컴포넌트 기능 완성

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,50 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
-export default function Input() {
-  return <div>Input</div>;
+export default function Input(props) {
+  const { type, title, inputId, border, borderRadius } = props;
+  return (
+    <>
+      <LabelStyle htmlFor={inputId} {...props}>
+        {title}
+      </LabelStyle>
+      <InputStyle
+        type={type ? type : "text"}
+        id={inputId}
+        border={border}
+        borderRadius={borderRadius}
+        {...props}
+      />
+    </>
+  );
 }
+
+const LabelStyle = styled.label`
+  color: var(--color-grey);
+  font-size: 16px;
+  margin: 6px 0 12px 0;
+`;
+
+const InputStyle = styled.input`
+  width: 480px;
+  padding: 20px 0;
+  font-size: 16px;
+  ${(props) =>
+    props.border
+      ? css`
+          border: ${props.border};
+        `
+      : css`
+          border-bottom: 1px solid var(--color-maingrey);
+        `}
+
+  ${(props) =>
+    props.borderRadius &&
+    css`
+      border-radius: ${props.borderRadius};
+    `}
+  &::placeholder {
+    color: var(--color-grey);
+  }
+  margin-bottom: 7px;
+`;
