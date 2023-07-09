792fa1d4dfaa0eabf739a36c700957f0ce023c0c
KIm Seo Young <jmh7367@naver.com>
2023-07-09 11:02:29 +0900

    Feat: ShoppingCartPage/MyPage Route 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import HomePage from "../pages/HomePage";
 import SignupPage from "../pages/SignupPage";
+import ShoppingCartPage from "../pages/ShoppingCartPage";
+import MyPage from "../pages/MyPage";
 
 export default function Router() {
   return (
@@ -10,6 +12,8 @@ export default function Router() {
       <Route path="/" element={<HomePage />} />
       <Route path="/loginpage" element={<LoginPage />} />
       <Route path="/signuppage" element={<SignupPage />} />
+      <Route path="/shoppingcartpage" element={<ShoppingCartPage />} />
+      <Route path="/mypage" element={<MyPage />} />
     </Routes>
   );
 }
