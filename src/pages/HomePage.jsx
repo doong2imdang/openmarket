5b0c20492f28b773024fbe817b665b23220988df
KIm Seo Young <jmh7367@naver.com>
2023-07-13 22:43:11 +0900

    Design: Header에 Carousel 컴포넌트 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Header from "../components/Header";
+import Carousel from "../components/Carousel";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Carousel />
+      </main>
+      <footer></footer>
+    </>
+  );
 }
