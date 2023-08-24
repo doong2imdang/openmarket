e64d1d9999a45bcf3733655e79bbf7e701dbaa1d
KIm Seo Young <jmh7367@naver.com>
2023-08-24 23:49:41 +0900

    Style: 필요없는 코드 삭제

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,20 +2,15 @@ import React from "react";
 import Header from "../components/Header";
 import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
-import styled from "styled-components";
 
 export default function HomePage() {
   return (
     <>
       <Header />
-      <Main>
+      <main>
         <Carousel />
-      </Main>
+      </main>
       <Footer />
     </>
   );
 }
-
-const Main = styled.main`
-  height: 500px;
-`;
