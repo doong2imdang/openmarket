2ef20f76445f1ec3213e53884fc937673ea93e1a
KIm Seo Young <jmh7367@naver.com>
2023-08-26 23:33:37 +0900

    Design: ProductList 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Carousel from "../components/Carousel";
+import ProductList from "../components/ProductList";
 import Footer from "../components/Footer";
 
 export default function HomePage() {
@@ -9,6 +10,7 @@ export default function HomePage() {
       <Header />
       <main>
         <Carousel />
+        <ProductList />
       </main>
       <Footer />
     </>
