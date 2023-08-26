a12667ddaf16e841da57143d3ecf527ee9a885d1
KIm Seo Young <jmh7367@naver.com>
2023-08-26 23:38:13 +0900

    Feat: 상품 전체 불러오기(GET)

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function ProductList() {
-  return <div>ProductList</div>;
+  const URL = "https://openmarket.weniv.co.kr";
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const handleGetProducts = async () => {
+      try {
+        const response = await fetch(URL + "/products");
+        if (!response.ok) {
+          throw new Error("HTTP error!");
+        }
+        const data = await response.json();
+        setProducts(data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    handleGetProducts();
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {products.map((product) => (
+          <li key={product.product_id}>
+            <img src={product.image} alt="판매상품" />
+            <span>{product.price}</span>
+            {product.product_name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
