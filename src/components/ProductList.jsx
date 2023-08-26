6b124c15cc56c28284d7b92090ffe1a6a014c1bb
KIm Seo Young <jmh7367@naver.com>
2023-08-26 23:49:51 +0900

    Design: ProductList style 추가

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 export default function ProductList() {
   const URL = "https://openmarket.weniv.co.kr";
@@ -23,15 +24,30 @@ export default function ProductList() {
 
   return (
     <>
-      <ul>
+      <UlStyle>
         {products.map((product) => (
           <li key={product.product_id}>
             <img src={product.image} alt="판매상품" />
+            <span>{product.store_name}</span>
+            <span>{product.product_name}</span>
             <span>{product.price}</span>
-            {product.product_name}
           </li>
         ))}
-      </ul>
+      </UlStyle>
     </>
   );
 }
+
+const UlStyle = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  place-items: center;
+
+  li {
+    display: flex;
+    flex-direction: column;
+  }
+  img {
+    max-width: 360px;
+  }
+`;
