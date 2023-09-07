c0717669bdee285d3ed6f6623190ddf879b59fbd
KIm Seo Young <jmh7367@naver.com>
2023-09-07 19:08:36 +0900

    Design: BuyNow페이지 기본 골격 완성

--- src/pages/BuyNow.jsx
+++ src/pages/BuyNow.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Header from "../components/Header";
+import styled from "styled-components";
+import { Main, UlStyle } from "./ShoppingCartPage";
 
 export default function BuyNow() {
-  return <div>BuyNow</div>;
+  return (
+    <>
+      <Header />
+      <Main>
+        <h1>주문/결제하기</h1>
+        <UlStylePlus>
+          <li>상품정보</li>
+          <li>할인</li>
+          <li>배송비</li>
+          <li>주문금액</li>
+        </UlStylePlus>
+      </Main>
+    </>
+  );
 }
+
+const UlStylePlus = styled(UlStyle)`
+  grid-template-columns: 2.5fr 1fr 1fr 1fr;
+`;
