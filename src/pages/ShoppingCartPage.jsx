f7e279bc22f8d28d46f7b6d6ded1443f6b14ab83
KIm Seo Young <jmh7367@naver.com>
2023-09-14 17:06:36 +0900

    Design: ShoppingCartPage 골격 완성

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -3,6 +3,8 @@ import Header from "../components/Header";
 import styled from "styled-components";
 import checkbox from "../assets/icon/cart-check-box.svg";
 import checkboxfill from "../assets/icon/cart-check-box-Fill.svg";
+import minusicon from "../assets/icon/icon-minus-line.svg";
+import plusicon from "../assets/icon/icon-plus-line.svg";
 import { useRecoilValue } from "recoil";
 import { userToken } from "../atom/loginAtom";
 import CartItem from "../components/CartItem";
@@ -64,15 +66,46 @@ export default function ShoppingCartPage() {
             <span>원하는 상품을 장바구니에 담아보세요!</span>
           </EmptyShoppingCart>
         ) : (
-          <div>
-            {cartItems.map((item) => (
-              <CartItem
-                key={item.product_id}
-                item={item}
-                isChecked={isChecked}
-              />
-            ))}
-          </div>
+          <>
+            <div>
+              {cartItems.map((item) => (
+                <CartItem
+                  key={item.product_id}
+                  item={item}
+                  isChecked={isChecked}
+                />
+              ))}
+            </div>
+            <TotalPrice>
+              <div>
+                <p>총 상품금액</p>
+                <strong>
+                  46,500<span>원</span>
+                </strong>
+              </div>
+              <img src={minusicon} alt="" />
+              <div>
+                <p>상품 할인</p>
+                <strong>
+                  0<span>원</span>
+                </strong>
+              </div>
+              <img src={plusicon} alt="" />
+              <div>
+                <p>배송비</p>
+                <strong>
+                  0<span>원</span>
+                </strong>
+              </div>
+              <div className="expectedTotalPrice">
+                <p>결제 예정 금액</p>
+                <strong>
+                  46,500<span>원</span>
+                </strong>
+              </div>
+            </TotalPrice>
+            <OrderBtn type="button">주문하기</OrderBtn>
+          </>
         )}
       </Main>
     </>
@@ -120,4 +153,67 @@ const EmptyShoppingCart = styled.div`
     font-size: 14px;
     color: var(--color-grey);
   }
+`;
+
+const TotalPrice = styled.div`
+  padding: 30px 50px;
+  width: 100%;
+  max-width: 1280px;
+  height: 150px;
+  background-color: var(--color-lightgrey);
+  display: grid;
+  margin: 80px auto 40px auto;
+  grid-template-columns: 1fr 0.5fr 1fr 0.5fr 1fr 1.5fr;
+  border-radius: 10px;
+  place-items: center;
+  text-align: center;
+  div {
+    padding: 0 10px;
+
+    p {
+      margin-bottom: 8px;
+    }
+
+    p,
+    span {
+      font-size: 16px;
+    }
+
+    strong {
+      font-size: 24px;
+      font-weight: bold;
+    }
+  }
+
+  .expectedTotalPrice {
+    p {
+      font-weight: bold;
+    }
+
+    strong {
+      color: var(--color-red);
+      font-size: 36px;
+      span {
+        font-size: 18px;
+      }
+    }
+  }
+
+  img {
+    display: block;
+    margin: auto;
+    background-color: #fff;
+    padding: 5px;
+    border-radius: 50px;
+  }
+`;
+
+const OrderBtn = styled.button`
+  display: block;
+  color: #fff;
+  background-color: var(--color-green);
+  width: 220px;
+  height: 68px;
+  border-radius: 5px;
+  margin: 0 auto;
 `;
